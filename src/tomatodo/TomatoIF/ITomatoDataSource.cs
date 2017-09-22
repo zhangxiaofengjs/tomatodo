2918adc0642843189170124b32bd22fe93d243a2
zhangxiaofengjs <zhangxiaofengjs@163.com>
2017-09-22 10:06:52 +0900

    add ignore bin/obj

---
+++ src/tomatodo/TomatoIF/ITomatoDataSource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace tomatodo.tomatoIF
+{
+	public interface ITomatoDataSource
+	{
+		TUser Login(string no, string pwd);
+		bool Insert(TodoItem[] items);
+		TCatagory[] GetCatagories(int userId);
+		TUser[] GetUsers();
+	}
+}
